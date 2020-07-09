e1b49273ae06324617bf46932c865c268e7c256b
saridjapeter <68054303+saridjapeter@users.noreply.github.com>
2020-07-09 13:51:25 +0300

    Добавлена версия и адрес сервера

--- install.ps1
+++ install.ps1
@@ -1,5 +1,10 @@
 ﻿$version = (Get-Content D:\Zabbix\zabbix_agentd.conf)[0] -replace ".*="
 $new_version = (Get-Content D:\Zabbix\install.bat)[68] -replace ".*="
 if ($version -lt $new_version) {
-
+$conf_file="D:\zabbix\zabbix_agentd.conf"
+New-Item -Path $conf_file -ItemType File -Force
+#Set-Content $conf_file "#Version=1.1"
+Add-content $conf_file "#Version=1.2" 
+Add-content $conf_file "Server=192.168.0.78"
+Add-content $conf_file "ServerActive=192.168.0.78"
 }
