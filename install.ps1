876eb93577502600545b0223f010a57a53ea25ce
saridjapeter <68054303+saridjapeter@users.noreply.github.com>
2020-07-13 13:56:53 +0300

    DETECT VIDEO SYSTEM

--- install.ps1
+++ install.ps1
@@ -13,5 +13,27 @@ $hostAD = Get-Content D:\zabbix\hostAD.ini
 $hostAD = $hostAD -split "@"
 $hostname = [string]::Concat("Hostname=", $hostAD[2], "-", $hostAD[1], "-", $hostAD[3])
 Add-content $conf_file $hostname
-Add-content $conf_file $hostname
+Add-content $conf_file "UserParameter=NAME[*],D:\zabbix\info.bat $1"
+Add-content $conf_file "UserParameter=INFO[*],D:\zabbix\$1"
+Add-content $conf_file "UserParameter=CASH[*],call D:\zabbix\KKM.bat $1"
+#-----/DETECT VIDEO SYSTEM/-----
+if (Test-Path -Path 'E:\video')
+   {
+    $HostMetadata = [string]::Concat("HostMetadata=SEVA", " ", $hostAD[2], " ", $hostAD[1], " ", $hostAD[0])
+    Add-content $conf_file $HostMetadata
+    #-----/Планировщик SEVA/-----
+    #Import-Module PSScheduledJob
+    #$Trigger = New-JobTrigger -Daily -At 05:00AM
+    #Register-ScheduledJob -Name SEVA_ARCHIVE -FilePath "D:\zabbix\Archive.ps1" -Trigger $Trigger
+    & SCHTASKS /Create /F /SC DAILY /ST 05:00 /TN SEVA_ARCHIVE /TR "PowerShell.exe -nologo -noninteractive -windowStyle hidden -File D:\zabbix\Archive.ps1"
+}
+if (Test-Path -Path 'E:\index')
+   {
+    $HostMetadata = [string]::Concat("HostMetadata=ALNET", " ", $hostAD[2], " ", $hostAD[1], " ", $hostAD[0])
+    Add-content $conf_file $HostMetadata
+    #-----/Планировщик ALNE/-----
+    & SCHTASKS /Create /F /SC HOURLY /TN ALNET_Self_test /TR D:\zabbix\alnet\day.bat
+}
+Add-content $conf_file "RefreshActiveChecks=60"
+Add-content $conf_file "LogFile=D:\zabbix\zabbix_agentd.log"
 }
